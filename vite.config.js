18e9ad2fc7c29a8ea8bea5b986f604a94ad48045
Daniel de Menezes Queiroz <danieldemenezesqueiroz@gmail.com>
2025-05-17 15:20:08 -0300

    Código-fonte original do site

--- vite.config.js
+++ vite.config.js
@@ -182,6 +182,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
+	base: '/danielz21discord.github.io/',
 	customLogger: logger,
 	plugins: [react(), addTransformIndexHtml],
 	server: {
